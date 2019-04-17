248c48a4d8253804f1965d04ed37dfebe8414acc
Abdullah <47503687+abdullahmafuz@users.noreply.github.com>
2019-04-18 00:32:21 +0400

    Updates

---
+++ precache-manifest.57fc14ef052fe384424e22e4ce8a4c6b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/portfolio_demo_ui/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "d05ca2fdcf206ca5f8ff",
+    "url": "/portfolio_demo_ui/static/css/main.8f98ddc8.chunk.css"
+  },
+  {
+    "revision": "ae5c6b093bc0330ceb3c",
+    "url": "/portfolio_demo_ui/static/js/runtime~main.6be0ed96.js"
+  },
+  {
+    "revision": "7aaa7867145dba5b9f7a",
+    "url": "/portfolio_demo_ui/static/js/2.8fec8d65.chunk.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/portfolio_demo_ui/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/portfolio_demo_ui/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/portfolio_demo_ui/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "d05ca2fdcf206ca5f8ff",
+    "url": "/portfolio_demo_ui/static/js/main.a86a0a07.chunk.js"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/portfolio_demo_ui/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "30fe6f02230799c31a3db6e4a25627d5",
+    "url": "/portfolio_demo_ui/static/media/hdbgvd.30fe6f02.mp4"
+  },
+  {
+    "revision": "431f60acaa4dcbb8e5985f239699f1db",
+    "url": "/portfolio_demo_ui/static/media/2.431f60ac.jpeg"
+  },
+  {
+    "revision": "8494021b4004681ce386233325a6c29f",
+    "url": "/portfolio_demo_ui/static/media/CV.8494021b.pdf"
+  },
+  {
+    "revision": "121f86da9d17f94516ca7b798ad89d92",
+    "url": "/portfolio_demo_ui/static/media/resumebg.121f86da.png"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/portfolio_demo_ui/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "c05bb5c1f5c7eb2ab9baaa1eddaf79f9",
+    "url": "/portfolio_demo_ui/index.html"
+  }
+];
